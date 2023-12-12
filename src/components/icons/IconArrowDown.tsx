7ce73d88f8d2065ec1ce03c578d401854662b702
Alejandro Coca <alxmcr@outlook.com>
2023-12-12 19:03:21 -0400

    wip

--- src/components/icons/IconArrowDown.tsx
+++ src/components/icons/IconArrowDown.tsx
@@ -1,18 +1,12 @@
 export default function IconArrowDown() {
   return (
-    <svg
-      width="26"
-      height="26"
-      viewBox="0 0 26 26"
-      fill="none"
-      xmlns="http://www.w3.org/2000/svg"
-    >
+    <svg xmlns="http://www.w3.org/2000/svg" width={26} height={26} fill="none">
       <path
-        d="M3 9L13 18L23 9"
         stroke="currentColor"
-        stroke-width="2"
-        stroke-linecap="round"
-        stroke-linejoin="round"
+        strokeLinecap="round"
+        strokeLinejoin="round"
+        strokeWidth={2}
+        d="m3 9 10 9 10-9"
       />
     </svg>
   );
