893eeffb1f73daa9a4e9f5c25001bdbded9ffd4c
Alejandro Coca <alxmcr@outlook.com>
2023-12-12 19:07:20 -0400

    wip

--- src/styles/globalStyles.ts
+++ src/styles/globalStyles.ts
@@ -4,4 +4,10 @@ export const GlobalStyles = createGlobalStyle`
   * {
     box-sizing: border-box;
   }
+
+  body {
+    background-color: #000;
+    font-family: 'Inter', sans-serif;
+    min-height: 100vh;
+  }
 `;
