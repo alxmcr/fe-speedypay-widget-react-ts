c9986c3457f18ff4905157b34673685cda15a00c
Alejandro Coca <alxmcr@outlook.com>
2023-12-12 15:59:19 -0400

    wip

---
+++ src/@types/appEnums.ts
@@ -0,0 +1,9 @@
+export enum IconNames {
+  arrowDown = 'icon-arrow-down',
+  arrowUp = 'icon-arrow-up',
+  bankTransfer = 'icon-bank-transfer',
+  cash = 'icon-cash',
+  creditCard = 'icon-credit-card',
+  email = 'icon-email',
+  gift = 'icon-gift',
+}
