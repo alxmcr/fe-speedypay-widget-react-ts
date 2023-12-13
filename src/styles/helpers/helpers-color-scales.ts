38dbad4a8b195482a2ec7e286ed694829486a9ce
Alejandro Coca <alxmcr@outlook.com>
2023-12-13 19:27:24 -0400

    wip

--- src/styles/helpers/helpers-color-scales.ts
+++ src/styles/helpers/helpers-color-scales.ts
@@ -1,4 +1,11 @@
-import { ATOLL_COLOR_SCALES } from '../colors-scales';
+import {
+  ATOLL_COLOR_SCALES,
+  IRON_COLOR_SCALES,
+  LIME_COLOR_SCALES,
+  SEA_BUCKTHORN_COLOR_SCALES,
+  VALHALLA_COLOR_SCALES,
+  WINE_BERRY_COLOR_SCALES,
+} from '../colors-scales';
 
 export const getScalesForColorsByCode = (codeColorScale = '') => {
   switch (codeColorScale) {
