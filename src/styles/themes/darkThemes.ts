cab03ede6c84d26277c3733a554a42232312b8ae
Alejandro Coca <alxmcr@outlook.com>
2023-12-13 19:13:26 -0400

    wip

--- src/styles/themes/darkThemes.ts
+++ src/styles/themes/darkThemes.ts
@@ -1,4 +1,9 @@
-import { CodesColorScales, ThemeModes } from '../../types/personalization/enumsPersonalization';
+import {
+  CodesColorScales,
+  ThemeModes,
+} from '../../types/personalization/enumsPersonalization';
+import { AppTheme } from '../../types/personalization/typesThemes';
+import { getThemeByColorScale } from '../helpers/helpers-theme';
 
 export const DARK_THEME_ATOLL: AppTheme = getThemeByColorScale(
   ThemeModes.dark,
--- src/styles/themes/lightThemes.ts
+++ src/styles/themes/lightThemes.ts
@@ -1,4 +1,9 @@
-import { CodesColorScales, ThemeModes } from '../../types/personalization/enumsPersonalization';
+import {
+  CodesColorScales,
+  ThemeModes,
+} from '../../types/personalization/enumsPersonalization';
+import { AppTheme } from '../../types/personalization/typesThemes';
+import { getThemeByColorScale } from '../helpers/helpers-theme';
 
 export const LIGHT_THEME_ATOLL: AppTheme = getThemeByColorScale(
   ThemeModes.light,
