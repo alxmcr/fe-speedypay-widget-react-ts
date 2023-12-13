5dda199e15a76a2bfbaff860734040b0cd1df7d0
Alejandro Coca <alxmcr@outlook.com>
2023-12-13 18:25:16 -0400

    wip

--- src/types/typesColors.ts
+++ src/types/typesColors.ts
@@ -11,3 +11,12 @@ export type ColorScales = {
   900: string;
   950: string;
 };
+
+export enum CodesColorScales {
+  atoll = 'atoll',
+  valhalla = 'valhalla',
+  lime = 'lime',
+  sea_buckthorn = 'sea_buckthorn',
+  wineBerry = 'wine_berry',
+  iron = 'iron',
+}
