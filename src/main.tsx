d460b0b9e6197a0ea723b34301dcb07f2bea0dde
Alejandro Coca <alxmcr@outlook.com>
2023-12-14 12:42:44 -0400

    wip

--- src/main.tsx
+++ src/main.tsx
@@ -1,12 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import App from './App.tsx';
 import AppProviders from './providers/AppProviders/index.tsx';
+import { RouterProvider, createBrowserRouter } from 'react-router-dom';
+import { routes } from './router/routes.tsx';
+
+const router = createBrowserRouter(routes);
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
     <AppProviders>
-      <App />
+      <RouterProvider router={router} />
     </AppProviders>
   </React.StrictMode>,
 );
---
+++ src/pages/HomePage/index.tsx
@@ -0,0 +1,20 @@
+import ButtonList from '../../components/buttons/ButtonList';
+import AppIconList from '../../components/icons/AppIcon/AppIconList';
+import ThemeModeToggler from '../../components/toggles/ThemeModeToggler';
+
+export default function HomePage() {
+  return (
+    <div
+      style={{
+        minHeight: '100vh',
+        display: 'flex',
+        flexDirection: 'column',
+        gap: '1rem',
+      }}
+    >
+      <ThemeModeToggler />
+      <AppIconList />
+      <ButtonList />
+    </div>
+  );
+}
