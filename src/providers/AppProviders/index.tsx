e8ade73a5001b3a7c579d67b31c5453a0dc6fe75
Alejandro Coca <alxmcr@outlook.com>
2023-12-13 23:31:30 -0400

    wip

---
+++ src/providers/AppProviders/index.tsx
@@ -0,0 +1,17 @@
+import ThemingProvider from '../ThemingProvider';
+import ToggleCodeColorScalesProvider from '../ToggleCodeColorScalesProvider';
+import ToggleModeThemeProvider from '../ToggleModeThemeProvider';
+
+type AppProvidersProps = {
+  children: React.ReactNode;
+};
+
+export default function AppProviders({ children }: AppProvidersProps) {
+  return (
+    <ToggleModeThemeProvider>
+      <ToggleCodeColorScalesProvider>
+        <ThemingProvider>{children}</ThemingProvider>
+      </ToggleCodeColorScalesProvider>
+    </ToggleModeThemeProvider>
+  );
+}
