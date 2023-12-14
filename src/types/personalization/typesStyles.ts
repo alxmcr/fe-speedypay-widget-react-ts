037bdfbf790d46e3825d0a0c936e1339ccc5786e
Alejandro Coca <alxmcr@outlook.com>
2023-12-14 00:32:53 -0400

    wip

--- src/types/personalization/typesStyles.ts
+++ src/types/personalization/typesStyles.ts
@@ -1,9 +1,25 @@
+import { ButtonVariants as ButtonStylesVariants } from './typeVariants';
+import { ThemeModes } from './typesThemes';
+
 export type CommonStyles = {
   background: string;
   borderColor: string;
   text: string;
   scrollBarBackground?: string;
   scrollBarTrackColor?: string;
+};
+
+export type AppCommonStyles = {
+  mode: ThemeModes;
+  background: string;
+  borderColor: string;
+  text: string;
+  scrollBarBackground?: string;
+  scrollBarTrackColor?: string;
+};
+
+export type AppStylesByElements = {
+  buttons: ButtonStylesVariants;
 };
 
 export type ButtonStyles = {
--- src/types/personalization/typesThemes.ts
+++ src/types/personalization/typesThemes.ts
@@ -1,21 +1,8 @@
-import { ButtonVariants } from './typeVariants';
+import { AppCommonStyles, AppStylesByElements } from './typesStyles';
 
 export enum ThemeModes {
   dark = 'dark',
   light = 'light',
 }
-
-export type AppCommonStyles = {
-  mode: ThemeModes;
-  background: string;
-  borderColor: string;
-  text: string;
-  scrollBarBackground?: string;
-  scrollBarTrackColor?: string;
-};
-
-export type AppStylesByElements = {
-  buttons: ButtonVariants;
-};
 
 export type AppTheme = AppCommonStyles & AppStylesByElements;
