0bd9e28217c75520eb62644bdd64fce761a37d90
Alejandro Coca <alxmcr@outlook.com>
2023-12-13 22:15:44 -0400

    wip

--- src/styles/helpers/helpers-theme.ts
+++ src/styles/helpers/helpers-theme.ts
@@ -1,13 +1,9 @@
-import { getBtnStylesByVariant } from '../../components/buttons/AppButton/AppBaseButton.styles';
-import {
-  CodeForBtnVariants,
-  CodesForColorScales,
-} from '../../types/personalization/enumsPersonalization';
+import { CodesForColorScales } from '../../types/personalization/enumsPersonalization';
 import { ColorScales } from '../../types/personalization/typesColors';
-import { ButtonStylesByState } from '../../types/personalization/typesStyles';
 import { AppTheme, ThemeModes } from '../../types/personalization/typesThemes';
 import { getColorScalesByCode } from './helpers-color-scales';
 import { getCommonStylesByColorScale } from './helpers-styles';
+import { getBtnVariants } from './helpers-styles-button';
 
 export const getThemeByColorScale = (
   theme: ThemeModes,
@@ -16,28 +12,11 @@ export const getThemeByColorScale = (
   const colorScales: ColorScales = getColorScalesByCode(codeColorScale);
   const commonStyles = getCommonStylesByColorScale(theme, colorScales);
 
-  // Button styles
-  const stylesBtnSolid: ButtonStylesByState = getBtnStylesByVariant(
-    theme,
-    colorScales,
-    CodeForBtnVariants.solid,
-  );
-  const stylesBtnOutline: ButtonStylesByState = getBtnStylesByVariant(
-    theme,
-    colorScales,
-    CodeForBtnVariants.outline,
-  );
-
-  const btnVariants = {
-    solid: stylesBtnSolid,
-    outline: stylesBtnOutline,
-  };
-
   // Theme
   const themeByColor: AppTheme = {
     mode: theme,
     ...commonStyles,
-    buttons: btnVariants,
+    buttons: getBtnVariants(theme, colorScales),
   };
 
   return themeByColor;
