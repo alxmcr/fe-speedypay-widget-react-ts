c51163afe2e08c19fd018cdd118da20f5b930a63
Alejandro Coca <alxmcr@outlook.com>
2023-12-14 00:50:39 -0400

    wip

--- src/styles/helpers/helpers-theme.ts
+++ src/styles/helpers/helpers-theme.ts
@@ -2,7 +2,7 @@ import { CodesForColorScales } from '../../types/personalization/enumsPersonaliz
 import { ColorScales } from '../../types/personalization/typesColors';
 import { AppTheme, ThemeModes } from '../../types/personalization/typesThemes';
 import { getColorScalesByCode } from './helpers-color-scales';
-import { getCommonStylesByColorScale } from './helpers-styles';
+import { getCommonStylesByColorScale } from './helpers-styles-common';
 import { getBtnVariants } from './helpers-styles-button';
 
 export const getThemeByColorScale = (
