0c3d3478978dd258cf9deef8c42a53745654644d
Alejandro Coca <alxmcr@outlook.com>
2023-12-13 23:53:05 -0400

    wip

--- src/providers/ThemingProvider/ThemingContext.tsx
+++ src/providers/ThemingProvider/ThemingContext.tsx
@@ -1,6 +1,12 @@
 import React from 'react';
-import { LIGHT_THEME_ATOLL } from '../../styles/themes/lightThemes';
-import { AppTheme } from '../../types/personalization/typesThemes';
+import { getThemeByColorScale } from '../../styles/helpers/helpers-theme';
+import { CodesForColorScales } from '../../types/personalization/enumsPersonalization';
+import { AppTheme, ThemeModes } from '../../types/personalization/typesThemes';
+
+export const LIGHT_THEME_ATOLL: AppTheme = getThemeByColorScale(
+  ThemeModes.light,
+  CodesForColorScales.atoll,
+);
 
 export type ThemingContextData = {
   themePersonalized: AppTheme;
--- src/providers/ThemingProvider/index.tsx
+++ src/providers/ThemingProvider/index.tsx
@@ -1,11 +1,13 @@
 import React from 'react';
 import { ThemeProvider } from 'styled-components';
 import { getThemeByColorScale } from '../../styles/helpers/helpers-theme';
-import { LIGHT_THEME_ATOLL } from '../../styles/themes/lightThemes';
-import { AppTheme } from '../../types/personalization/typesThemes';
 import { ToggleCodeColorScalesContext } from '../ToggleCodeColorScalesProvider/ToggleCodeColorScalesContext';
 import { ToggleModeThemeContext } from '../ToggleModeThemeProvider/ToggleModeThemeContext';
-import { ThemingContext, ThemingContextData } from './ThemingContext';
+import {
+  LIGHT_THEME_ATOLL,
+  ThemingContext,
+  ThemingContextData,
+} from './ThemingContext';
 
 type ThemingProviderProps = {
   children: React.ReactNode;
@@ -16,7 +18,7 @@ export default function ThemingProvider({ children }: ThemingProviderProps) {
   const { codeColorScales } = React.useContext(ToggleCodeColorScalesContext);
 
   const [themePersonalized, setThemePersonalized] =
-    React.useState<AppTheme>(LIGHT_THEME_ATOLL);
+    React.useState(LIGHT_THEME_ATOLL);
 
   const value: ThemingContextData = {
     themePersonalized,
