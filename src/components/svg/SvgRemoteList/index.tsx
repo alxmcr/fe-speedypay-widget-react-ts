8862031f79ccc6cb30c778e44e145dd12533a01a
Alejandro Coca <alxmcr@outlook.com>
2023-12-14 22:24:55 -0400

    wip

---
+++ src/components/svg/SvgRemoteList/index.tsx
@@ -0,0 +1,25 @@
+import SvgRemote from '../SvgRemote';
+
+export default function SvgRemoteList() {
+  const BASE_URL = 'http://localhost:6006';
+
+  return (
+    <div>
+      <SvgRemote
+        url={`${BASE_URL}/logos/Logo-Fitters.svg`}
+        width={154}
+        height={42}
+      />
+      <SvgRemote
+        url={`${BASE_URL}/logos/Logo-Finberry.svg`}
+        width={154}
+        height={42}
+      />
+      <SvgRemote
+        url={`${BASE_URL}/logos/Logo-Betsson.svg`}
+        width={154}
+        height={42}
+      />
+    </div>
+  );
+}
