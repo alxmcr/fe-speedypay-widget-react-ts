ebf53d50af002c5efc61add3eaccd659db18bc05
Alejandro M. Coca <alxmcr@outlook.com>
2023-12-16 19:24:46 -0400

    wip

---
+++ src/mock/mock-checkout.ts
@@ -0,0 +1,27 @@
+import { DbCheckout } from '../types/serviceTypes';
+import {
+  mockCompanyBetsson,
+  mockCompanyFinberry,
+  mockCompanyFittersOriginal,
+} from './mock-companies';
+
+export const checkout001: DbCheckout = {
+  ch_checkout: 'checkout-001',
+  ch_company: mockCompanyBetsson,
+  ch_amount_to_pay: 1265.89,
+  ch_currency_amount: '$',
+};
+
+export const checkout002: DbCheckout = {
+  ch_checkout: 'checkout-002',
+  ch_company: mockCompanyFinberry,
+  ch_amount_to_pay: 787.36,
+  ch_currency_amount: 'MXN',
+};
+
+export const checkout003: DbCheckout = {
+  ch_checkout: 'checkout-003',
+  ch_company: mockCompanyFittersOriginal,
+  ch_amount_to_pay: 859.12,
+  ch_currency_amount: 'USD',
+};
--- src/types/serviceTypes.ts
+++ src/types/serviceTypes.ts
@@ -1,6 +1,19 @@
+export type DbCompany = {
+  co_company: string;
+  co_name: string;
+  co_logo_url: string;
+};
+
 export type DbPaymentMethod = {
   pm_payment_method: string;
   pm_code: 'cash' | 'bank_transfer' | 'card' | 'card';
   pm_name: string;
   pm_is_disabled: boolean;
+};
+
+export type DbCheckout = {
+  ch_checkout: string;
+  ch_amount_to_pay: number;
+  ch_currency_amount: string;
+  ch_company: DbCompany;
 };
