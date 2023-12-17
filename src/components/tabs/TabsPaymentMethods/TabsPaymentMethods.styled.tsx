dae75daac06de35b8dc6bfc1c3028a10dc97830a
Alejandro M. Coca <alxmcr@outlook.com>
2023-12-17 14:42:07 -0400

    wip

--- src/components/tabs/TabsPaymentMethods/TabsPaymentMethods.styled.tsx
+++ src/components/tabs/TabsPaymentMethods/TabsPaymentMethods.styled.tsx
@@ -7,21 +7,21 @@ export const TabsPaymentMethodsStyled = styled.div`
   padding: 0.5rem 0;
 
   &::-webkit-scrollbar-track {
-    box-shadow: inset 0 0 1px ${(props) => props.theme.scrollBarTrackColor};
+    box-shadow: inset 0 0 1px ${(props) => props.theme.styles.scrollBarTrackColor};
     -webkit-box-shadow: inset 0 0 1px
-      ${(props) => props.theme.scrollBarTrackColor};
-    background-color: ${(props) => props.theme.scrollBarBackground};
+      ${(props) => props.theme.styles.scrollBarTrackColor};
+    background-color: ${(props) => props.theme.styles.scrollBarBackground};
   }
 
   &::-webkit-scrollbar {
     height: 4px;
     width: 5px;
-    background-color: ${(props) => props.theme.scrollBarBackground};
+    background-color: ${(props) => props.theme.styles.scrollBarBackground};
   }
 
   &::-webkit-scrollbar-thumb {
     height: 4px;
     width: 1px;
-    background-color: ${(props) => props.theme.scrollBarTrackColor};
+    background-color: ${(props) => props.theme.styles.scrollBarTrackColor};
   }
 `;
