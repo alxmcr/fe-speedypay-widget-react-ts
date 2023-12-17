7512b25bb9438d47cc4a1085a38ed7b490ead479
Alejandro M. Coca <alxmcr@outlook.com>
2023-12-17 13:29:29 -0400

    wip

--- src/components/boxes/BoxCardInfoElements/BoxCardInfoElements.styled.tsx
+++ src/components/boxes/BoxCardInfoElements/BoxCardInfoElements.styled.tsx
@@ -1,6 +1,6 @@
 import styled from 'styled-components';
 
-export const BoxContactElementsStyled = styled.div`
+export const BoxCardInfoElementsStyled = styled.div`
   display: flex;
   flex-direction: column;
   gap: 1.75rem;
--- src/components/boxes/BoxContactElements/index.tsx
+++ src/components/boxes/BoxContactElements/index.tsx
@@ -1,9 +1,11 @@
 import FormElementsContactInfo from '../../form-fields-groups/FormElementsContactInfo';
+import { SubtitleOne } from '../../typography/Subtitles';
 import { BoxContactElementsStyled } from './BoxContactElements.styled';
 
 export default function BoxContactElements() {
   return (
     <BoxContactElementsStyled>
+      <SubtitleOne>Contact information</SubtitleOne>
       <FormElementsContactInfo />
     </BoxContactElementsStyled>
   );
