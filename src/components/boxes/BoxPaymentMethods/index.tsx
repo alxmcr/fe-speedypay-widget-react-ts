6888c3d59ae2639606708d235068768276712fd1
Alejandro M. Coca <alxmcr@outlook.com>
2023-12-17 14:56:04 -0400

    wip

--- src/components/boxes/BoxPaymentMethods/index.tsx
+++ src/components/boxes/BoxPaymentMethods/index.tsx
@@ -1,47 +1,21 @@
-import React from 'react';
-import { LoadingStates } from '../../../helpers/constants/constants-services';
-import { CheckoutContext } from '../../../providers/checkout/CheckoutProvider/CheckoutContext';
+import { PaymentMethod } from '../../../types/appTypes';
 import TabsPaymentMethods from '../../tabs/TabsPaymentMethods';
-import { BodyTextTwo } from '../../typography/BodyTexts';
 import { SubtitleOne } from '../../typography/Subtitles';
 import { BoxPaymentMethodsStyled } from './BoxPaymentMethods.styled';
 
 type BoxPaymentMethodsProps = {
   subtitle: string;
+  paymentMethods: PaymentMethod[];
 };
 
 export default function BoxPaymentMethods({
   subtitle = '',
+  paymentMethods = [],
 }: BoxPaymentMethodsProps) {
-  const { checkout, loadingCheckout } = React.useContext(CheckoutContext);
-
-  if (LoadingStates.PENDING === loadingCheckout) {
-    return (
-      <BoxPaymentMethodsStyled>
-        <SubtitleOne>{subtitle}</SubtitleOne>
-        <BodyTextTwo>Loading...</BodyTextTwo>
-      </BoxPaymentMethodsStyled>
-    );
-  }
-
-  if (LoadingStates.SUCCESS === loadingCheckout) {
-    if (checkout === null) {
-      return (
-        <BoxPaymentMethodsStyled>
-          <SubtitleOne>{subtitle}</SubtitleOne>
-          <BodyTextTwo>No checkout</BodyTextTwo>
-        </BoxPaymentMethodsStyled>
-      );
-    } else {
-      if (checkout.payment_methods?.length > 0) {
-        return (
-          <BoxPaymentMethodsStyled>
-            <SubtitleOne>{subtitle}</SubtitleOne>
-            <TabsPaymentMethods paymentMethods={checkout.payment_methods} />
-          </BoxPaymentMethodsStyled>
-        );
-      }
-    }
-  }
-  return null;
+  return (
+    <BoxPaymentMethodsStyled>
+      <SubtitleOne>{subtitle}</SubtitleOne>
+      <TabsPaymentMethods paymentMethods={paymentMethods} />
+    </BoxPaymentMethodsStyled>
+  );
 }
