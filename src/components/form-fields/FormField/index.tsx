8202cb68278abab301b73649140ffcfbd769c578
Alejandro M. Coca <alxmcr@outlook.com>
2023-12-17 18:32:02 -0400

    wip

--- src/components/form-fields/FormField/index.tsx
+++ src/components/form-fields/FormField/index.tsx
@@ -5,17 +5,17 @@ type FormFieldProps = {
   children: React.ReactNode;
   id: string;
   labelText: string;
-  width?: string;
+  $width?: string;
 };
 
 export default function FormField({
   id = 'form-field-example',
   labelText = 'Label',
-  width = '100%',
+  $width = '50%',
   children,
 }: FormFieldProps) {
   return (
-    <FormFieldStyled width={width}>
+    <FormFieldStyled $width={$width}>
       <TextLabel htmlFor={id}>{labelText}</TextLabel>
       {children}
     </FormFieldStyled>
