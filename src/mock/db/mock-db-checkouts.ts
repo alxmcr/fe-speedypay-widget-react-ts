81046f9bbba1b2cf21ea5498b45d42ce8c645469
Alejandro M. Coca <alxmcr@outlook.com>
2023-12-17 01:59:11 -0400

    wip

--- src/mock/db/mock-db-checkouts.ts
+++ src/mock/db/mock-db-checkouts.ts
@@ -10,8 +10,14 @@ import {
   DbCreditCardSample,
 } from './mock-db-payment-methods';
 
+export const CHECKOUTS_ID = {
+  id001: 'checkout-001',
+  id002: 'checkout-002',
+  id003: 'checkout-003',
+};
+
 export const dbCheckout001: DbCheckout = {
-  ch_checkout: 'checkout-001',
+  ch_checkout: CHECKOUTS_ID.id001,
   ch_company: dbCompanyBetsson,
   ch_amount_to_pay: 1265.89,
   ch_currency_amount: '$',
@@ -19,7 +25,7 @@ export const dbCheckout001: DbCheckout = {
 };
 
 export const dbCheckout002: DbCheckout = {
-  ch_checkout: 'checkout-002',
+  ch_checkout: CHECKOUTS_ID.id002,
   ch_company: dbCompanyFinberry,
   ch_amount_to_pay: 787.36,
   ch_currency_amount: 'MXN',
@@ -27,7 +33,7 @@ export const dbCheckout002: DbCheckout = {
 };
 
 export const dbCheckout003: DbCheckout = {
-  ch_checkout: 'checkout-003',
+  ch_checkout: CHECKOUTS_ID.id003,
   ch_company: dbFittersOriginal,
   ch_amount_to_pay: 859.12,
   ch_currency_amount: 'USD',
