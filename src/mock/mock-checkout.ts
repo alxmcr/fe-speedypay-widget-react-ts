c59eaeb39fecaeab5dc1ef9e88633f745fa32d88
Alejandro M. Coca <alxmcr@outlook.com>
2023-12-16 21:53:49 -0400

    wip

--- src/mock/mock-checkout.ts
+++ src/mock/mock-checkout.ts
@@ -4,14 +4,18 @@ import {
   mockCompanyFinberry,
   mockCompanyFittersOriginal,
 } from './mock-companies';
-import { DbCashSample } from './mock-db-payment-methods';
+import {
+  DbBankTransferSample,
+  DbCashSample,
+  DbCreditCardSample,
+} from './mock-db-payment-methods';
 
 export const checkout001: DbCheckout = {
   ch_checkout: 'checkout-001',
   ch_company: mockCompanyBetsson,
   ch_amount_to_pay: 1265.89,
   ch_currency_amount: '$',
-  ch_payment_methods: [DbCashSample],
+  ch_payment_methods: [DbCreditCardSample, DbBankTransferSample, DbCashSample],
 };
 
 export const checkout002: DbCheckout = {
@@ -19,7 +23,7 @@ export const checkout002: DbCheckout = {
   ch_company: mockCompanyFinberry,
   ch_amount_to_pay: 787.36,
   ch_currency_amount: 'MXN',
-  ch_payment_methods: [],
+  ch_payment_methods: [DbCreditCardSample, DbCashSample],
 };
 
 export const checkout003: DbCheckout = {
@@ -27,5 +31,5 @@ export const checkout003: DbCheckout = {
   ch_company: mockCompanyFittersOriginal,
   ch_amount_to_pay: 859.12,
   ch_currency_amount: 'USD',
-  ch_payment_methods: [],
+  ch_payment_methods: [DbCashSample],
 };
