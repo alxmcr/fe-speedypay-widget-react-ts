a5f70d226bd0d4cde304eff43d24f574b3167ae5
Alejandro M. Coca <alxmcr@outlook.com>
2023-12-17 18:05:24 -0400

    wip

--- src/mock/db/mock-db-payment-methods.ts
+++ src/mock/db/mock-db-payment-methods.ts
@@ -1,29 +1,35 @@
 import { DbPaymentMethod } from '../../types/serviceTypes';
 
+export const PAYMENT_METHODS_CODES = {
+  cash: 'cash',
+  card: 'card',
+  bankTransfer: 'bank_transfer',
+};
+
 export const DbBankTransferSample: DbPaymentMethod = {
   pm_payment_method: 'pay-method-001',
-  pm_code: 'bank_transfer',
+  pm_code: PAYMENT_METHODS_CODES.bankTransfer,
   pm_name: 'Bank transfer',
   pm_is_disabled: false,
 };
 
 export const DbCashSample: DbPaymentMethod = {
   pm_payment_method: 'pay-method-002',
-  pm_code: 'cash',
+  pm_code: PAYMENT_METHODS_CODES.cash,
   pm_name: 'Cash',
   pm_is_disabled: false,
 };
 
 export const DbCashSampleDisabled: DbPaymentMethod = {
   pm_payment_method: 'pay-method-002-1',
-  pm_code: 'cash',
+  pm_code: PAYMENT_METHODS_CODES.cash,
   pm_name: 'Cash',
   pm_is_disabled: true,
 };
 
 export const DbCreditCardSample: DbPaymentMethod = {
   pm_payment_method: 'pay-method-003',
-  pm_code: 'card',
+  pm_code: PAYMENT_METHODS_CODES.card,
   pm_name: 'Card',
   pm_is_disabled: false,
 };
