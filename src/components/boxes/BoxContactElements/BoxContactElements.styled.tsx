34936426e16204fa83392f7e699a4df0069ab8b8
Alejandro M. Coca <alxmcr@outlook.com>
2023-12-17 13:17:55 -0400

    wip

---
+++ src/components/boxes/BoxContactElements/BoxContactElements.styled.tsx
@@ -0,0 +1,5 @@
+import styled from 'styled-components';
+
+export const BoxContactElementsStyled = styled.div`
+  display: flex;
+`;
---
+++ src/components/boxes/BoxContactElements/index.tsx
@@ -0,0 +1,10 @@
+import FormElementsContactInfo from '../../form-fields-groups/FormElementsContactInfo';
+import { BoxContactElementsStyled } from './BoxContactElements.styled';
+
+export default function BoxContactElements() {
+  return (
+    <BoxContactElementsStyled>
+      <FormElementsContactInfo />
+    </BoxContactElementsStyled>
+  );
+}
