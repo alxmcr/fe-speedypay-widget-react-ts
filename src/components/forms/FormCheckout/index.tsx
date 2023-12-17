eaeb0a3da544c507db3fefd0c889380f26e957e8
Alejandro M. Coca <alxmcr@outlook.com>
2023-12-17 18:00:53 -0400

    wip

--- src/components/forms/FormCheckout/index.tsx
+++ src/components/forms/FormCheckout/index.tsx
@@ -1,3 +1,4 @@
+import React from 'react';
 import { Checkout } from '../../../types/appTypes';
 import BoxCardInfoElements from '../../boxes/BoxCardInfoElements';
 import BoxContactInfoElements from '../../boxes/BoxContactInfoElements';
