e4d9c20f766c510cc731b66d0bbad2eaabfd360b
Alejandro M. Coca <alxmcr@outlook.com>
2023-12-18 12:43:45 -0400

    wip

--- src/components/form-fields-groups/FormElementsContactInfo.tsx
+++ src/components/form-fields-groups/FormElementsContactInfo.tsx
@@ -1,25 +1,41 @@
+import React from 'react';
 import FormField from '../form-fields/FormField';
-import AppInput from '../inputs/common/AppInput';
+import { AppInputStyled } from '../inputs/common/AppInput/AppInput.styled';
 import { FormFieldsGroupStyled } from './FormFieldsGroup.styled';
 
 export default function FormElementsContactInfo() {
+  const [email, setEmail] = React.useState('');
+  const [fullname, setFullname] = React.useState('');
+
+  const handleEmail = (ev: React.ChangeEvent<HTMLInputElement>) => {
+    setEmail(ev.target.value);
+  };
+
+  const handleFullname = (ev: React.ChangeEvent<HTMLInputElement>) => {
+    setFullname(ev.target.value);
+  };
+
   return (
     <FormFieldsGroupStyled $flexDirection="column">
       <FormField labelText="Email" id="email" $width="100%">
-        <AppInput
+        <AppInputStyled
           type="email"
           id="email"
           placeholder="Enter your email"
-          fullWidth={true}
+          $fullWidth={true}
+          value={email}
+          onChange={handleEmail}
           required
         />
       </FormField>
       <FormField labelText="Full name" id="full-name" $width="100%">
-        <AppInput
+        <AppInputStyled
           type="text"
           id="full-name"
           placeholder="Enter your first and last name"
-          fullWidth={true}
+          $fullWidth={true}
+          value={fullname}
+          onChange={handleFullname}
           required
         />
       </FormField>
