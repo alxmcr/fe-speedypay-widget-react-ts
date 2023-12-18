d3fa479f1d97f46df44196402efb396179aea91f
Alejandro M. Coca <alxmcr@outlook.com>
2023-12-18 12:50:59 -0400

    wip

--- src/components/form-fields-groups/FormElementsCardDetails.tsx
+++ src/components/form-fields-groups/FormElementsCardDetails.tsx
@@ -1,20 +1,41 @@
+import React from 'react';
 import FormField from '../form-fields/FormField';
-import AppInputWithMask from '../inputs/AppInputWithMask';
+import { AppInputWithMaskStyled } from '../inputs/AppInputWithMask/AppInputWithMask.styled';
 import { FormFieldsGroupRowStyled } from './FormFieldsGroupRow.styled';
 
 export default function FormElementsCardDetails() {
+  const [expirationDate, setExpirationDate] = React.useState('');
+  const [cvc, setCvc] = React.useState('');
+
+  const handleExpirationDate = (ev: React.ChangeEvent<HTMLInputElement>) => {
+    setExpirationDate(ev.target.value);
+  };
+
+  const handleCvc = (ev: React.ChangeEvent<HTMLInputElement>) => {
+    setCvc(ev.target.value);
+  };
+
   return (
     <FormFieldsGroupRowStyled>
       <FormField labelText="Expiration date" id="expiration-date">
-        <AppInputWithMask
+        <AppInputWithMaskStyled
           id="expiration-date"
           mask="99/99"
           placeholder="MM/DD"
+          value={expirationDate}
+          onChange={handleExpirationDate}
           required
         />
       </FormField>
       <FormField labelText="CVC" id="CVC">
-        <AppInputWithMask id="CVC" mask="999" placeholder="CVC" required />
+        <AppInputWithMaskStyled
+          id="CVC"
+          mask="999"
+          placeholder="CVC"
+          value={cvc}
+          onChange={handleCvc}
+          required
+        />
       </FormField>
     </FormFieldsGroupRowStyled>
   );
