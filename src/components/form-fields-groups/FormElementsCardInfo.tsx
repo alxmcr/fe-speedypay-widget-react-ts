8fa2e0eb28fb5257536d7a9b505c0d58c442fdfd
Alejandro M. Coca <alxmcr@outlook.com>
2023-12-18 13:08:25 -0400

    wip

--- src/components/form-fields-groups/FormElementsCardInfo.tsx
+++ src/components/form-fields-groups/FormElementsCardInfo.tsx
@@ -1,19 +1,37 @@
-import { mockInstallmentsFull } from '../../mock/mock-installments';
+import React from 'react';
 import FormField from '../form-fields/FormField';
-import AppInputWithMask from '../inputs/AppInputWithMask';
-import AppInput from '../inputs/common/AppInput';
-import AppSelect from '../selects/AppSelect';
+import { AppInputWithMaskStyled } from '../inputs/AppInputWithMask/AppInputWithMask.styled';
+import { AppInputStyled } from '../inputs/common/AppInput/AppInput.styled';
 import FormElementsCardDetails from './FormElementsCardDetails';
 import { FormFieldsGroupStyled } from './FormFieldsGroup.styled';
 
 export default function FormElementsCardInfo() {
+  const [cardNumber, setCardNumber] = React.useState('');
+  const [cardHolderName, setCardHolderName] = React.useState('');
+
+  const [installmentSelected, setInstallmentSelected] = React.useState('');
+
+  const handleCardNumber = (ev: React.ChangeEvent<HTMLInputElement>) => {
+    setCardNumber(ev.target.value);
+  };
+
+  const handleCardHolderName = (ev: React.ChangeEvent<HTMLInputElement>) => {
+    setCardHolderName(ev.target.value);
+  };
+
+  const handleOptionInstallments = (ev: React.ChangeEvent<HTMLSelectElement>) => {
+    setInstallmentSelected(ev.target.value);
+  };
+
   return (
     <FormFieldsGroupStyled $flexDirection="column">
       <FormField labelText="Card number" id="card-number" $width="100%">
-        <AppInputWithMask
+        <AppInputWithMaskStyled
           id="card-number"
           mask="9999 9999 9999 9999"
           placeholder="Enter your card number"
+          value={cardNumber}
+          onChange={handleCardNumber}
           required
         />
       </FormField>
@@ -23,11 +41,13 @@ export default function FormElementsCardInfo() {
         id="card-holder-name"
         $width="100%"
       >
-        <AppInput
+        <AppInputStyled
           type="text"
           id="card-holder-name"
           placeholder="Enter card holder name"
-          fullWidth={true}
+          $fullWidth={true}
+          value={cardHolderName}
+          onChange={handleCardHolderName}
         />
       </FormField>
       <FormField
@@ -35,13 +55,15 @@ export default function FormElementsCardInfo() {
         id="select-installments"
         $width="100%"
       >
-        <AppSelect
+        {/* <AppSelect
           id="select-installments"
           name="select-installments"
           placeholder="Choose an installment"
           options={mockInstallmentsFull}
+          value={installmentSelected}
+          onChange={handleOptionInstallments}
           width="100%"
-        />
+        /> */}
       </FormField>
     </FormFieldsGroupStyled>
   );
