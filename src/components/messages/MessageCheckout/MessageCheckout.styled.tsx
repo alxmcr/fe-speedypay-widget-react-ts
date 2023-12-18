61afa90113493921279fb451fd8160d25eb12ef7
Alejandro M. Coca <alxmcr@outlook.com>
2023-12-17 23:31:33 -0400

    wip

--- src/components/messages/MessageCheckout/MessageCheckout.styled.tsx
+++ src/components/messages/MessageCheckout/MessageCheckout.styled.tsx
@@ -3,10 +3,10 @@ import styled from 'styled-components';
 export const MessageCheckoutStyled = styled.article`
   border-radius: 0.5rem;
   background-color: ${(props) =>
-    props.theme.styles.messages.default.background};
+    props.theme.styles.messages.default.backgroundColor};
   border: 1px solid
     ${(props) => props.theme.styles.messages.default.borderColor};
-  color: ${(props) => props.theme.styles.messages.default.text};
+  color: ${(props) => props.theme.styles.messages.default.colorText};
   padding: 0.625rem 0.75rem;
 
   align-items: flex-start;
