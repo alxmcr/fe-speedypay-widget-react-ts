1e61a80315293da3592294c39d8f079b1bb844ed
Alejandro M. Coca <alxmcr@outlook.com>
2023-12-19 01:48:38 -0400

    wip

--- src/pages/HomePage/index.tsx
+++ src/pages/HomePage/index.tsx
@@ -2,14 +2,15 @@ import CheckoutTemplate from '../../components/@templates/CheckoutTemplate';
 import ThemeModeToggler from '../../components/toggles/ThemeModeToggler';
 
 export default function HomePage() {
-
   return (
     <div
       style={{
-        minHeight: '100vh',
+        alignItems: 'center',
         display: 'flex',
         flexDirection: 'column',
         gap: '1rem',
+
+        minHeight: '100vh',
       }}
     >
       <ThemeModeToggler />
