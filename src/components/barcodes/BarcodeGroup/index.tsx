76c8ba9b4e5567935c781f19c8223304c1728925
Alejandro Coca <alxmcr@outlook.com>
2023-12-21 19:46:59 -0400

    wip

--- src/components/barcodes/BarcodeGroup/index.tsx
+++ src/components/barcodes/BarcodeGroup/index.tsx
@@ -1,5 +1,10 @@
+import { ThemeContext } from 'styled-components';
 import { BodyTextOne } from '../../typography/BodyTexts';
 import Barcode from '../Barcode';
+import React from 'react';
+import { ToggleModeThemeContext } from '../../../providers/personalization/ToggleModeThemeProvider/ToggleModeThemeContext';
+import { ToggleCodeColorScalesContext } from '../../../providers/personalization/ToggleCodeColorScalesProvider/ToggleCodeColorScalesContext';
+import { ThemingContext } from '../../../providers/personalization/ThemingProvider/ThemingContext';
 
 type BarcodeGroupProps = {
   text: string;
@@ -8,9 +13,11 @@ type BarcodeGroupProps = {
 };
 
 export default function BarcodeGroup({ text = '' }: BarcodeGroupProps) {
+  const { themePersonalized } = React.useContext(ThemingContext);
+
   return (
     <div>
-      <Barcode text={text} barcolor="#ff8565" />
+      <Barcode text={text} barcolor={themePersonalized.styles.colorText} />
       <BodyTextOne>{text}</BodyTextOne>
     </div>
   );
