ec4e55dce8988c4ae10e0eabf8bc0334c866a83d
Alejandro M. Coca <alxmcr@users.noreply.github.com>
2023-12-21 19:28:17 -0400

    Feature/24 results page (#39)

---
+++ src/pages/ResultsPage/index.tsx
@@ -0,0 +1,20 @@
+import ThemeModeToggler from '../../components/toggles/ThemeModeToggler';
+import { HeadingOne } from '../../components/typography/Headings';
+
+export default function ResultsPage() {
+  return (
+    <div
+      style={{
+        alignItems: 'center',
+        display: 'flex',
+        flexDirection: 'column',
+        gap: '1rem',
+
+        minHeight: '100vh',
+      }}
+    >
+      <HeadingOne>Results</HeadingOne>
+      <ThemeModeToggler />
+    </div>
+  );
+}
--- src/router/routes.tsx
+++ src/router/routes.tsx
@@ -1,10 +1,16 @@
 import { RouteObject } from 'react-router-dom';
 import HomePage from '../pages/HomePage';
+import ResultsPage from '../pages/ResultsPage';
 
 export const routes: RouteObject[] = [
   {
     id: 'home-page',
     path: '/',
     element: <HomePage />,
+  },
+  {
+    id: 'results-page',
+    path: '/results',
+    element: <ResultsPage />,
   },
 ];
