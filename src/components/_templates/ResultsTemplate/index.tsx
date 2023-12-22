76b0498ae7e836d42a05932a7f6391bb7b8ec9dc
Alejandro Coca <alxmcr@outlook.com>
2023-12-22 12:02:57 -0400

    wip

--- src/components/_templates/ResultsTemplate/index.tsx
+++ src/components/_templates/ResultsTemplate/index.tsx
@@ -1,13 +1,17 @@
 import React from 'react';
+import {
+  BANK_TRANSFER_ONE_STEP,
+  BANK_TRANSFER_THREE_STEP,
+  BANK_TRANSFER_TWO_STEP,
+  CASH_ONE_STEP,
+  CASH_THREE_STEP,
+  CASH_TWO_STEP
+} from '../../../helpers/constants/constants-steps';
 import { CheckoutContext } from '../../../providers/checkout/CheckoutProvider/CheckoutContext';
-import { AppTemplateStyled } from '../AppTemplate.styled';
+import { NumberNames } from '../../../types/enumsApp';
 import BarcodeGroup from '../../barcodes/BarcodeGroup';
 import AppStep from '../../steps/AppStep';
-import { NumberNames } from '../../../types/enumsApp';
-import {
-  BANK_TRANSFER_STEPS,
-  CASH_STEPS,
-} from '../../../helpers/constants/constants-steps';
+import { AppTemplateStyled } from '../AppTemplate.styled';
 
 export default function ResultsTemplate() {
   const { checkout } = React.useContext(CheckoutContext);
@@ -16,15 +20,15 @@ export default function ResultsTemplate() {
     <AppTemplateStyled>
       <BarcodeGroup text={checkout?.id || '13123'} />
       <div>
-        <AppStep text={CASH_STEPS.one} stepNumber={NumberNames.one} />
-        <AppStep text={CASH_STEPS.two} stepNumber={NumberNames.two} />
-        <AppStep text={CASH_STEPS.three} stepNumber={NumberNames.three} />
+        <AppStep text={CASH_ONE_STEP} stepNumber={NumberNames.one} />
+        <AppStep text={CASH_TWO_STEP} stepNumber={NumberNames.two} />
+        <AppStep text={CASH_THREE_STEP} stepNumber={NumberNames.three} />
       </div>
       <div>
-        <AppStep text={BANK_TRANSFER_STEPS.one} stepNumber={NumberNames.one} />
-        <AppStep text={BANK_TRANSFER_STEPS.two} stepNumber={NumberNames.two} />
+        <AppStep text={BANK_TRANSFER_ONE_STEP} stepNumber={NumberNames.one} />
+        <AppStep text={BANK_TRANSFER_TWO_STEP} stepNumber={NumberNames.two} />
         <AppStep
-          text={BANK_TRANSFER_STEPS.three}
+          text={BANK_TRANSFER_THREE_STEP}
           stepNumber={NumberNames.three}
         />
       </div>
--- src/helpers/constants/constants-steps.ts
+++ src/helpers/constants/constants-steps.ts
@@ -1,13 +1,11 @@
-export const BANK_TRANSFER_STEPS = {
-  one: 'Ingresa a tu portal digital de tu banco',
-  two: 'Copia la CLABE proporcionada.',
-  three:
-    'Transfiere desde tu banca en línea a esta CLABE Interbancaria para completar tu compra. ',
-};
+export const BANK_TRANSFER_ONE_STEP =
+  'Ingresa a tu portal digital de tu banco.';
+export const BANK_TRANSFER_TWO_STEP = 'Copia la CLABE proporcionada.';
+export const BANK_TRANSFER_THREE_STEP =
+  'Transfiere desde tu banca en línea a esta CLABE Interbancaria para completar tu compra.';
 
-export const CASH_STEPS = {
-  one: 'Visita el OXXO más cercano.',
-  two: 'Menciona que realizarás un pago en efectivo con OXXO Pay.',
-  three:
-    'Realiza tu pago usando la referencia antes de la fecha de expiración.',
-};
+export const CASH_ONE_STEP = 'Visita el OXXO más cercano.';
+export const CASH_TWO_STEP =
+  'Menciona que realizarás un pago en efectivo con OXXO Pay.';
+export const CASH_THREE_STEP =
+  'Realiza tu pago usando la referencia antes de la fecha de expiración.';
