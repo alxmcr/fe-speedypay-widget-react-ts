bae8dc24a8edae4d7bff6f35ef3f31f9092eba3f
Alejandro Coca <alxmcr@outlook.com>
2023-12-22 00:54:14 -0400

    wip

--- src/components/_templates/ResultsTemplate/index.tsx
+++ src/components/_templates/ResultsTemplate/index.tsx
@@ -4,6 +4,10 @@ import { AppTemplateStyled } from '../AppTemplate.styled';
 import BarcodeGroup from '../../barcodes/BarcodeGroup';
 import AppStep from '../../steps/AppStep';
 import { NumberNames } from '../../../types/enumsApp';
+import {
+  BANK_TRANSFER_STEPS,
+  CASH_STEPS,
+} from '../../../helpers/constants/constants-steps';
 
 export default function ResultsTemplate() {
   const { checkout } = React.useContext(CheckoutContext);
@@ -11,8 +15,19 @@ export default function ResultsTemplate() {
   return (
     <AppTemplateStyled>
       <BarcodeGroup text={checkout?.id || '13123'} />
-      <AppStep text="equwieuioqw" stepNumber={NumberNames.one} />
-      <AppStep text="vcxvxcvx" stepNumber={NumberNames.two} />
+      <div>
+        <AppStep text={CASH_STEPS.one} stepNumber={NumberNames.one} />
+        <AppStep text={CASH_STEPS.two} stepNumber={NumberNames.two} />
+        <AppStep text={CASH_STEPS.three} stepNumber={NumberNames.three} />
+      </div>
+      <div>
+        <AppStep text={BANK_TRANSFER_STEPS.one} stepNumber={NumberNames.one} />
+        <AppStep text={BANK_TRANSFER_STEPS.two} stepNumber={NumberNames.two} />
+        <AppStep
+          text={BANK_TRANSFER_STEPS.three}
+          stepNumber={NumberNames.three}
+        />
+      </div>
     </AppTemplateStyled>
   );
 }
