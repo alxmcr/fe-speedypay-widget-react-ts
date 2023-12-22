43fcb6e7f5c49e09c3ddcec56a614001f78faa92
Alejandro Coca <alxmcr@outlook.com>
2023-12-22 00:41:37 -0400

    wip

---
+++ src/helpers/constants/constants-steps.ts
@@ -0,0 +1,13 @@
+export const BANK_TRANSFER_STEPS = {
+  one: 'Ingresa a tu portal digital de tu banco',
+  two: 'Copia la CLABE proporcionada.',
+  three:
+    'Transfiere desde tu banca en línea a esta CLABE Interbancaria para completar tu compra. ',
+};
+
+export const CASH_STEPS = {
+  one: 'Visita el OXXO más cercano.',
+  two: 'Menciona que realizarás un pago en efectivo con OXXO Pay.',
+  three:
+    'Realiza tu pago usando la referencia antes de la fecha de expiración.',
+};
