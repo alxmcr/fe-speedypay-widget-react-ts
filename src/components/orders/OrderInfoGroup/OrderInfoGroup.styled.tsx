9c3183a87265c6e0c0ebd24a5bd3885feaf926b1
Alejandro Coca <alxmcr@outlook.com>
2023-12-23 18:00:01 -0400

    wip

--- src/components/orders/OrderInfoGroup/OrderInfoGroup.styled.tsx
+++ src/components/orders/OrderInfoGroup/OrderInfoGroup.styled.tsx
@@ -1,7 +1,11 @@
 import styled from 'styled-components';
 
 export const OrderInfoGroupStyled = styled.div`
+  border: 1px solid red;
+  border-radius: 0.5rem;
+
   display: flex;
   flex-direction: column;
   gap: 1rem;
+  padding: 1.625rem;
 `;
