b98f541a0af0f8df0a9cf4a9a7575d98b1aed026
Alejandro Coca <alxmcr@outlook.com>
2023-12-24 14:07:42 -0400

    wip

--- src/components/charges/BoxChargeSuccess/index.tsx
+++ src/components/charges/BoxChargeSuccess/index.tsx
@@ -1,9 +1,10 @@
 import IconBigCheck from '../../_icons/big/IconBigCheck';
+import { BoxChargeSuccessStyled } from './BoxChargeSuccess.styled';
 
 export default function BoxChargeSuccess() {
   return (
-    <div>
+    <BoxChargeSuccessStyled>
       <IconBigCheck />
-    </div>
+    </BoxChargeSuccessStyled>
   );
 }
