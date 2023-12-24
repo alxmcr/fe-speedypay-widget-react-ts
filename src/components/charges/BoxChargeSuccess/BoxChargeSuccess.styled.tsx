3ddd2a4029233413579ee623f29aebb58659d112
Alejandro M. Coca <alxmcr@users.noreply.github.com>
2023-12-24 14:10:57 -0400

    Feature/41 charge success (#56)

---
+++ src/components/charges/BoxChargeSuccess/BoxChargeSuccess.styled.tsx
@@ -0,0 +1,8 @@
+import styled from 'styled-components';
+
+export const BoxChargeSuccessStyled = styled.article`
+  align-items: center;
+  display: flex;
+  flex-direction: column;
+  gap: 0.5rem;
+`;
