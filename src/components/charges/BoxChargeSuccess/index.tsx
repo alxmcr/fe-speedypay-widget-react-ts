8f45493eec0099c898eace41d946aeb75a097dfe
Alejandro Coca <alxmcr@outlook.com>
2023-12-24 14:08:38 -0400

    wip

--- src/components/charges/BoxChargeSuccess/index.tsx
+++ src/components/charges/BoxChargeSuccess/index.tsx
@@ -1,10 +1,14 @@
 import IconBigCheck from '../../_icons/big/IconBigCheck';
+import { BodyTextOne } from '../../_typography/BodyTexts';
+import { HeadingSix } from '../../_typography/Headings';
 import { BoxChargeSuccessStyled } from './BoxChargeSuccess.styled';
 
 export default function BoxChargeSuccess() {
   return (
     <BoxChargeSuccessStyled>
       <IconBigCheck />
+      <HeadingSix>Success</HeadingSix>
+      <BodyTextOne>Your payment has been confirmed!</BodyTextOne>
     </BoxChargeSuccessStyled>
   );
 }
