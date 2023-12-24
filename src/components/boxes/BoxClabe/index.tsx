a70191a413ef926c186c45e0280e0df15a6fcc98
Alejandro Coca <alxmcr@outlook.com>
2023-12-23 23:40:37 -0400

    wip

---
+++ src/components/boxes/BoxClabe/index.tsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import { BoxPaymentReferenceStyled } from '../BoxPaymentReference/BoxPaymentReference.styled';
+import { OrderContext } from '../../../providers/order/OrderProvider/OrderContext';
+import { HeadingSix } from '../../_typography/Headings';
+import BarcodeGroup from '../../barcodes/BarcodeGroup';
+
+export default function BoxClabe() {
+  const { order } = React.useContext(OrderContext);
+
+  return (
+    <BoxPaymentReferenceStyled>
+      <HeadingSix>Clabe</HeadingSix>
+      <BarcodeGroup text={order?.reference_number || ''} />
+    </BoxPaymentReferenceStyled>
+  );
+}
