fd0eab0e794fa0dbc687e3e9937a5ebdf066be00
Alejandro Coca <alxmcr@outlook.com>
2023-12-25 14:57:49 -0400

    wip

---
+++ src/styles/colors/black-haze.ts
@@ -0,0 +1,15 @@
+import { ScalesForColors } from '../../types/personalization/typesColors';
+
+export const BLACK_HAZE: ScalesForColors = {
+  50: '#f7fafc',
+  100: '#e8f0f6',
+  200: '#cce0eb',
+  300: '#a0c7d9',
+  400: '#6ca9c4',
+  500: '#4a8ead',
+  600: '#387391',
+  700: '#2e5c76',
+  800: '#294f63',
+  900: '#274353',
+  950: '#1a2b37',
+};
--- src/styles/colors/index.ts
+++ src/styles/colors/index.ts
@@ -4,5 +4,6 @@ import { LIME } from './lime';
 import { VALHALLA } from './valhalla';
 import { SEA_BUCKTHORN } from './sea-buckthorn';
 import { WINE_BERRY } from './wine-berry';
+import { BLACK_HAZE } from './black-haze';
 
-export { ATOLL, IRON, LIME, SEA_BUCKTHORN, VALHALLA, WINE_BERRY };
+export { ATOLL, IRON, LIME, SEA_BUCKTHORN, VALHALLA, WINE_BERRY, BLACK_HAZE };
