67af151c4afa9a78c9cfd4263c0d4bcaaa2f7fd9
Alejandro Coca <alxmcr@outlook.com>
2023-12-24 21:00:07 -0400

    wip

--- src/components/_templates/AppTemplate.styled.tsx
+++ src/components/_templates/AppTemplate.styled.tsx
@@ -1,7 +1,6 @@
 import styled from 'styled-components';
 
 export const AppTemplateStyled = styled.article`
-  border: 1px solid red;
   border-radius: 0.5rem;
   display: flex;
   flex-direction: column;
