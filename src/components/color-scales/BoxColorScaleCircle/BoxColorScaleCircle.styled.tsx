7a9780d7c770c0c0c8f375b016da642810c9d506
Alejandro Coca <alxmcr@outlook.com>
2023-12-25 23:56:11 -0400

    wip

--- src/components/color-scales/BoxColorScaleCircle/BoxColorScaleCircle.styled.tsx
+++ src/components/color-scales/BoxColorScaleCircle/BoxColorScaleCircle.styled.tsx
@@ -3,4 +3,8 @@ import styled from 'styled-components';
 export const BoxColorScaleCircleStyled = styled.div`
   height: 4.375rem;
   width: 4.375rem;
+
+  align-items: center;
+  display: flex;
+  justify-content: center;
 `;
--- src/components/color-scales/GroupColorScales/GroupColorScales.styled.tsx
+++ src/components/color-scales/GroupColorScales/GroupColorScales.styled.tsx
@@ -1,6 +1,7 @@
 import styled from 'styled-components';
 
 export const GroupColorScalesStyled = styled.div`
+  align-items: center;
   display: flex;
   gap: 1rem;
 `;
