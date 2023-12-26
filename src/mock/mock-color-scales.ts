cff846731e0ad474476a85659c641338afc666bb
Alejandro Coca <alxmcr@outlook.com>
2023-12-25 23:31:23 -0400

    wip

---
+++ src/mock/mock-color-scales.ts
@@ -0,0 +1,12 @@
+import {
+    LIGHT_COLOR_SCALES,
+    SEA_BUCKTHORN_COLOR_SCALES,
+    VALHALLA_COLOR_SCALES,
+} from '../styles/colors-scales';
+import { ColorScales } from '../types/personalization/typesColors';
+
+export const mockColorScales: ColorScales[] = [
+  LIGHT_COLOR_SCALES,
+  SEA_BUCKTHORN_COLOR_SCALES,
+  VALHALLA_COLOR_SCALES,
+];
