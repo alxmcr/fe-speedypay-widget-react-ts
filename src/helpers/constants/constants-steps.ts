a84b8074b726ec47c5ea19822fb0155bd85680b2
Alejandro M. Coca <alxmcr@users.noreply.github.com>
2023-12-26 19:50:26 -0400

    Fix/53 change messages (#68)

--- src/helpers/constants/constants-steps.ts
+++ src/helpers/constants/constants-steps.ts
@@ -3,33 +3,33 @@ import { ResultStep } from '../../types/appTypes';
 export const BANK_TRANSFER_ONE_STEP: ResultStep = {
   id: 'bank-transfer-1',
   code: '1',
-  name: 'Ingresa a tu portal digital de tu banco.',
+  name: 'Enter to your bank digital.',
 };
 export const BANK_TRANSFER_TWO_STEP: ResultStep = {
   id: 'bank-transfer-2',
   code: '2',
-  name: 'Copia la CLABE proporcionada.',
+  name: 'Copy CLABE shared.',
 };
 export const BANK_TRANSFER_THREE_STEP: ResultStep = {
   id: 'bank-transfer-3',
   code: '3',
-  name: 'Transfiere desde tu banca en línea a esta CLABE Interbancaria para completar tu compra.',
+  name: 'Transfer from your bank digital using the CLABE for complete your shopping.',
 };
 
 export const CASH_ONE_STEP: ResultStep = {
   id: 'cash-1',
   code: '1',
-  name: 'Visita el OXXO más cercano.',
+  name: 'Visit some nearest Cash store.',
 };
 export const CASH_TWO_STEP: ResultStep = {
   id: 'cash-2',
   code: '2',
-  name: 'Menciona que realizarás un pago en efectivo con OXXO Pay.',
+  name: 'Mention that you want to make a cash payment.',
 };
 export const CASH_THREE_STEP: ResultStep = {
   id: 'cash-3',
   code: '3',
-  name: 'Realiza tu pago usando la referencia antes de la fecha de expiración.',
+  name: 'Use payment reference number generated for complete your shopping.',
 };
 
 export const BANK_TRANSFER_STEPS = [
